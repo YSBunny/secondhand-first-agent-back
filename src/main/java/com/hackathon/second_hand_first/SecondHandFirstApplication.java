b499e0790279422960d39ffd842b79cd043e1379
seul_bunny <yun0836@naver.com>
2026-08-19 19:26:07 +0900

    init: first commit

---
+++ src/main/java/com/hackathon/second_hand_first/SecondHandFirstApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.second_hand_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondHandFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/second_hand_first/SecondHandFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.second_hand_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
